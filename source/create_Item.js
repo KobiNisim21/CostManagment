62cce929d0d6ac51376626723afd69061585fa6d
Kobin <kobinisim21@gmail.com>
2023-01-06 11:12:41 +0200

    added a popup window to the form

--- source/create_Item.js
+++ source/create_Item.js
@@ -1,30 +1,23 @@
 function init() {
+  let name = document.getElementById("fname").value;
+  let sum = document.getElementById("Price").value;
+  let category = document.getElementById("browser").value;
+  let date = document.getElementById("Date").value;
 
-    let name = document.getElementById("fname").value;
-    let sum = document.getElementById("Price").value;
-    let category = document.getElementById("browser").value;
-    let date = document.getElementById("Date").value;
-
-    if (name != "" && sum != "" && category != "" && date != "") {
-
-        if (localStorage.getItem("i") == null) {
-            i = 1
-        }
-
-        else {
-            i = localStorage.getItem("i");
-        } 
-        
-        createItem(name, sum, category, date, i);  
+  if (name != "" && sum != "" && category != "" && date != "") {
+    if (localStorage.getItem("i") == null) {
+      i = 1;
+    } else {
+      i = localStorage.getItem("i");
     }
+
+    createItem(name, sum, category, date, i);
+  }
 }
-
-
 
 function createItem(name, sum, category, date, i) {
-    const storage = new LocalStorageWrapper();
-    storage.addItem(name, sum, category, date, i);
-    i++;
-    localStorage.setItem("i", i);
+  const storage = new LocalStorageWrapper();
+  storage.addItem(name, sum, category, date, i);
+  i++;
+  localStorage.setItem("i", i);
 }
-
